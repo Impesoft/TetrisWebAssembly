ec9b4798a7ea0baa19f7fb35a6b1a4ace466c4e4
Ward Impe <Ward@impesoft.com>
2026-08-01 15:49:39 +0200

    Add PWA update support with Toolbelt.Blazor.PWA.Updater
    Integrated Toolbelt.Blazor.PWA.Updater for PWA update notifications:
    - Added NuGet package and registered AddPWAUpdater() in Program.cs
    - Imported updater in _Imports.razor and added <PWAUpdater /> to MainLayout
    - Updated service-worker.published.js to handle SKIP_WAITING for immediate activation

--- BlazorArcade/Program.cs
+++ BlazorArcade/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Globalization;
 using BlazorArcade;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
@@ -10,4 +11,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddPWAUpdater();
 await builder.Build().RunAsync();
