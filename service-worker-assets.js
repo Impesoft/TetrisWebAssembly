8dde700c24bed7e59d0b5d3279420e076b6f3ce9
Impesoft <Impesoft@users.noreply.github.com>
2026-08-05 04:25:41 +0000

    deploy: bb0cfa783e70a5739ad59746239daaf3ee4d1950

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "xLEjfrwF",
+  "version": "qoIqOp6k",
   "assets": [
     {
       "hash": "sha256-mtmFQcDI2/gGDQv7pGCRI23Mu3tFr3Znh2Ugtdy4ejU=",
@@ -18,8 +18,8 @@ self.assetsManifest = {
       "url": "_content/Toolbelt.Blazor.PWA.Updater/Toolbelt.Blazor.PWA.Updater.2drl8nn3fe.bundle.scp.css"
     },
     {
-      "hash": "sha256-G7WLjPHDo09iSvWK5b1SzP2MCCSTTMy1QKhSrGOfjRQ=",
-      "url": "_framework/BlazorArcade.3ixe48j1tr.wasm"
+      "hash": "sha256-D6PQFOTnNIe6Szxion1rnTGDgNpZ+CNglSsuU7jP6zo=",
+      "url": "_framework/BlazorArcade.5fhbw48wsj.wasm"
     },
     {
       "hash": "sha256-h5dswxgcv3htHnD3+CJjR8xh0Lvkyv3SKLqZ1ygQ294=",
@@ -218,7 +218,7 @@ self.assetsManifest = {
       "url": "_framework/blazor.webassembly.js"
     },
     {
-      "hash": "sha256-ytNN0eulK0ZIIuL0lVVCYZStKHN9/NIxflz6ldvGrcw=",
+      "hash": "sha256-Y7ixHR1mFRbNIKrNfA7PFhg5RnG0giARCyVsW3+sLms=",
       "url": "_framework/dotnet.js"
     },
     {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: xLEjfrwF */
+/* Manifest version: qoIqOp6k */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
